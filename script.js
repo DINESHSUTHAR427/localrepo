1b895203c83c02ffead2672353d0d511fc4316c0
DINESH <dineshsutharr427@gmail.com>
2024-09-19 17:00:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-//  function calcaddition(num1,num2){
-//     return num1 + num2;
-//  }
-//  console.log(calcaddition(6,7));
+ function calcaddition(num1,num2){
+    return num1 + num2;
+ }
+ console.log(calcaddition(6,7));
  
-//  
-// const a = ["hydrogen", "helium", "lithium", "beryllium"];
-// normal funstions
-// const a2 = a.map(function(s){
-//     return s.length;
-// })
-//  console.log("normale way ", a2)
-
-
-
-//  arrowfunstion
-// const a2  = a. map((s) => s.length)
-// console.log("arrow way", a2)
  
-//  calling funstion
-//  EX1 function weicomemsg (name) {
-//     return ("hello"+ name +"welcome to geekstogeeks");
-//  console.log(welcomemsg(nameval));
-//  }
-//  let nameval = "admin";
-//  Ex
-// function product(a,b){
-//     return a * b;
-// }
-// let result= product.call(this,20,38);
-// console.log(result);  // double breact lickne liye hai value print karne keliye nahi  
-
-// let employee = {
-
-//         name: "Rahul",
-//         deparment: "sales",
-//         details: () => {
-//             return this .empname +
-//             "work with deparment" +
-//             this.department;
-//         }
-// };
-// console.log(employee.details());
+const a = ["hydrogen", "helium", "lithium", "beryllium"];
+normal funstions
+const a2 = a.map(function(s){
+    return s.length;
+})
+ console.log("normale way ", a2)
 
 
 
-// how to write
-// function name (arguments){
-//     javascript statements
-// }
+ arrowfunstion
+const a2  = a. map((s) => s.length)
+console.log("arrow way", a2)
+ 
+ calling funstion
+ EX1 function weicomemsg (name) {
+    return ("hello"+ name +"welcome to geekstogeeks");
+ console.log(welcomemsg(nameval));
+ }
+ let nameval = "admin";
+ Ex
+function product(a,b){
+    return a * b;
+}
+let result= product.call(this,20,38);
+console.log(result);  // double breact lickne liye hai value print karne keliye nahi  
+
+let employee = {
+
+        name: "Rahul",
+        deparment: "sales",
+        details: () => {
+            return this .empname +
+            "work with deparment" +
+            this.department;
+        }
+};
+console.log(employee.details());
+
+
+
+how to write
+function name (arguments){
+    javascript statements
+}
  
 
